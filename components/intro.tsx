724d7046878c98b6af6b7a9a70200ca7ce1aa2d2
KamrAnDarmAn <KamrAnDarmAn72@gmail.com>
2025-03-19 10:51:30 +0430

    s commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,8 +31,8 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
+              src="/profile.JPEG"
+              alt="K DarmAn"
               width="192"
               height="192"
               quality="95"
@@ -51,9 +51,7 @@ export default function Intro() {
               delay: 0.1,
               duration: 0.7,
             }}
-          >
-            👋
-          </motion.span>
+          ></motion.span>
         </div>
       </div>
 
@@ -62,11 +60,19 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        {/* <span className="font-bold">Hello, I'm KamrAn.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">React (Next.js)</span> */}
+        <span className="font-bold">Hello, I'm KamrAn DarmAn.</span>
+        <span className="font-bold">
+          {" "}
+          I'm a full-stack web developer passionate about building{" "}
+          <span className="italic">sites and apps</span>.
+        </span>{" "}
+        My focus is <span className="underline">React (Next.js)</span> and
+        backend development with Prisma, MySQL, Mongodb and PostgreSQL."
       </motion.h1>
 
       <motion.div
@@ -100,7 +106,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="www.linkedin.com/in/kamran-darman"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +114,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/KamrAnDarmAn"
           target="_blank"
         >
           <FaGithubSquare />
--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import portfolioAppImg from "@/public/porto.png";
+import socialmediaAppImg from "@/public/social.png";
+import blogAppImg from "@/public/blog.png";
 
 export const links = [
   {
@@ -35,52 +35,59 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Graduated Graduated from Institute of Computer Science",
+    location: "Khost, Afghanistan",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I completed a 2-year program in Computer Science, specializing in databases, including MySQL, SQL Server, and PL/SQL.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2024",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "DSA Enthusiast & Problem Solver",
+    location: "Online (Udemy, YouTube)",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "I am currently focused on Data Structures and Algorithms (DSA), continuously improving my problem-solving skills and deepening my understanding of algorithms. My goal is to strengthen my knowledge for real-world applications and prepare for software engineering roles.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2024 - Present",
   },
   {
     title: "Full-Stack Developer",
-    location: "Houston, TX",
+    location: "Online (Udemy, YouTube)",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I am also skilled in full-stack development, working with React, Next.js, TypeScript, Tailwind, Prisma, and MySQL. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2023 - 2024",
   },
 ] as const;
 
+
 export const projectsData = [
   {
-    title: "CorpComment",
+    // title: "CorpComment",
+    // description:
+    //   "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    // tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    // imageUrl: corpcommentImg,
+    title: "Blog App",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "I built a full-stack blog application where users can create, edit, and delete blog posts. The app includes user authentication and a simple commenting system.",
+    tags: ["React", "Next.js", "Prisma", "MySQL", "Tailwind"],
+    imageUrl:blogAppImg,
+
   },
   {
-    title: "rmtDev",
+    title: "Social Media App",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "I developed a social media application where users can post updates, like, and comment on posts. It includes features like user profiles, a news feed, and direct messaging.",
+    tags: ["React", "Next.js", "Prisma", "SQL Server", "Tailwind"],
+    imageUrl: socialmediaAppImg,
   },
   {
-    title: "Word Analytics",
+    title: "Portfolio App",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "I created a personal portfolio app to showcase my skills, projects, and resume. The app features an interactive UI and integrates with a contact form.",
+    tags: ["React", "Next.js", "Prisma", "Tailwind"],
+    imageUrl:portfolioAppImg,
   },
 ] as const;
 
@@ -98,10 +105,7 @@ export const skillsData = [
   "MongoDB",
   "Redux",
   "GraphQL",
-  "Apollo",
   "Express",
   "PostgreSQL",
-  "Python",
-  "Django",
   "Framer Motion",
 ] as const;
